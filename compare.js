d85f59b53e74b43578f3dc26b31e78d93770e863
mikolalysenko <mikolalysenko@gmail.com>
2015-06-20 13:28:02 -0500

    factor out orientation test

--- compare.js
+++ compare.js
@@ -1,36 +1,10 @@
 'use strict'
 
 var compareCells = require('compare-cell')
+var parity = require('cell-orientation')
 
 module.exports = compareOrientedCells
 
-function parity(s) {
-  var p = 1
-  for(var i=1; i<s.length; ++i) {
-    for(var j=0; j<i; ++j) {
-      if(s[j] < s[i]) {
-        p = -p
-      } else if(s[j] === s[i]) {
-        return 0
-      }
-    }
-  }
-  return p
-}
-
 function compareOrientedCells(a, b) {
-  var n = a.length, m = b.length
-  if(n !== m) {
-    return n - m
-  }
-  switch(n) {
-    case 0:
-      return 0
-    case 1:
-      return a[0] - b[0]
-    case 2:
-      return (a[0] - b[0]) || (a[1] - b[1])
-    default:
-      return compareCells(a, b) || (parity(a) - parity(b))
-  }
+  return compareCells(a, b) || parity(a) - parity(b)
 }
